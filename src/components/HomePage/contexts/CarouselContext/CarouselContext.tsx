14711acba7a1e59dcea54ef05b03d8ebbf36e57c
Michal Rygorowicz <mitsugi23@hotmail.com>
2024-04-19 16:36:46 +0200

    #31 stop carousel on mouseover

--- src/components/HomePage/contexts/CarouselContext/CarouselContext.tsx
+++ src/components/HomePage/contexts/CarouselContext/CarouselContext.tsx
@@ -11,42 +11,66 @@ interface CarouselBarContext {
     activeImage: number;
     setActiveImage: React.Dispatch<SetStateAction<number>>;
     setProgress: React.Dispatch<SetStateAction<number>>;
+    startCarousel: () => void;
+    stopCarousel: () => void;
 }
 
+const MAX_PROGRESS = 100;
+const MIN_PROGRESS = 0;
+const LAST_IMG_IDX = NUM_IMAGES - 1;
+const FIRST_IDX = 0;
+
+function nextIndex(previousIdx: number) {
+    return previousIdx + 1;
+}
 const getStep = (interval: number, slideLengthMs: number) => {
     return 100 / (slideLengthMs / interval);
 };
-
 const step = getStep(CONFIG.CAROUSEL.INTERVAL, CONFIG.CAROUSEL.SLIDE_LENGTH_MS);
 
 const CarouselContext = createContext({} as CarouselBarContext);
 
 const CarouselContextProvider = ({ children }: CarouselContextProps) => {
+    const [isRunning, setIsRunning] = useState(true);
     const [activeImage, setActiveImage] = useState(0);
     const [progress, setProgress] = useState(0);
 
+    function stopCarousel() {
+        setProgress(0);
+        setIsRunning(false);
+    }
+
+    function startCarousel() {
+        setIsRunning(true);
+    }
+
     useEffect(() => {
+        if (!isRunning) return;
+
         const progressInterval = setInterval(() => {
             setProgress((prev) => {
-                if (prev < 100) {
+                if (prev < MAX_PROGRESS) {
                     return prev + step;
                 } else {
                     setActiveImage((prev) => {
-                        if (prev < NUM_IMAGES - 1) {
-                            return prev + 1;
+                        if (prev < LAST_IMG_IDX) {
+                            return nextIndex(prev);
                         } else {
-                            return 0;
+                            return FIRST_IDX;
                         }
                     });
-                    return 0;
+
+                    return MIN_PROGRESS;
                 }
             });
         }, CONFIG.CAROUSEL.INTERVAL);
+
         return () => clearInterval(progressInterval);
-    }, []);
+    }, [isRunning]);
 
     return (
-        <CarouselContext.Provider value={{ activeImage, progress, setProgress, setActiveImage }}>
+        <CarouselContext.Provider
+            value={{ activeImage, progress, setProgress, setActiveImage, startCarousel, stopCarousel }}>
             {children}
         </CarouselContext.Provider>
     );

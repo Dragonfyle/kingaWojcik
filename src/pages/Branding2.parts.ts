d61d4361b2d9725cbcd244f0e64d140210e2b305
Michal Rygorowicz <mitsugi23@hotmail.com>
2024-05-11 12:38:40 +0200

    hotfix

--- src/pages/Branding2.parts.ts
+++ src/pages/Branding2.parts.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import ukenPattern from "/src/assets/icons/uken_pattern_element.svg";
+import ukenPattern from "src/assets/icons/uken_pattern_element.svg";
 
 export const Wrapper = styled.div`
     display: flex;

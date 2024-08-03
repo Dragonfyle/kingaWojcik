adbc4f8ad1dae2ac600232965954f1fa2688ad82
Michal Rygorowicz <mitsugi23@hotmail.com>
2024-08-03 13:04:20 +0200

    style improvements

--- src/components/HomePage/ProjectsSection/NavButtons/NavButtons.tsx
+++ src/components/HomePage/ProjectsSection/NavButtons/NavButtons.tsx
@@ -1,13 +1,28 @@
-import * as P from "./NavButtons.parts";
 import NextIcon from "$generics/NavIcon/Next/Next";
 import PreviousIcon from "$generics/NavIcon/Previous/Previous";
 import { NavButtonsProps } from "./NavButtons.types";
+import { useThemeContext } from "$contexts/ThemeContext";
+import * as P from "./NavButtons.parts";
 
 export default function NavButtons({ onNextProject, onPreviousProject, isFirstIndex, isLastIndex }: NavButtonsProps) {
+    const {
+        theme: { colors },
+    } = useThemeContext();
+
     return (
         <P.ButtonsWrapper>
-            <PreviousIcon $size="50px" onClick={onPreviousProject} isDisabled={isFirstIndex} />
-            <NextIcon $size="100px" onClick={onNextProject} isDisabled={isLastIndex} />
+            <PreviousIcon
+                $color={colors.leading.secondary[2]}
+                $size="50px"
+                onClick={onPreviousProject}
+                isDisabled={isFirstIndex}
+            />
+            <NextIcon
+                $color={colors.leading.secondary[2]}
+                $size="100px"
+                onClick={onNextProject}
+                isDisabled={isLastIndex}
+            />
         </P.ButtonsWrapper>
     );
 }

8346c9a7bbc1248c5217d082745073353f4a0c8f
Kinga Wojcik <kinga.wojcik14@gmail.com>
2024-09-30 18:54:00 +0200

    change of font sizes

--- src/components/HomePage/ProjectsSection/ProjectPanel/ProjectPanelIntro/ProjectPanelIntro.tsx
+++ src/components/HomePage/ProjectsSection/ProjectPanel/ProjectPanelIntro/ProjectPanelIntro.tsx
@@ -4,7 +4,7 @@ import { ProjectPanelIntroProps } from "./ProjectPanelIntro.types";
 export default function ProjectPanelIntro({ text }: ProjectPanelIntroProps) {
     return (
         <div className="mr-50px my-30px flex w-11/12 snap-start flex-col s:mb-0 s:min-w-project-panel-item l:mt-0">
-            <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-6 [&_p]:text-ms [&_p]:s:text-m">
                 <TinaMarkdown content={text} />
             </div>
         </div>
--- src/components/ProjectPage/ColumnDescription/ColumnDescription.tsx
+++ src/components/ProjectPage/ColumnDescription/ColumnDescription.tsx
@@ -11,10 +11,10 @@ export function ColumnDescription({ header, description, descriptionPosition }: 
 
     return (
         <div className={`${orderUtility} flex flex-col gap-10`}>
-            {hasHeader && <h3 className="text-2xl font-bold leading-tight">{header}</h3>}
+            {hasHeader && <h3 className="text-2xl font-bold leading-tight [&_p]:text-m [&_p]:s:text-2xl">{header}</h3>}
 
             {hasDescription && (
-                <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-4 [&_p]:text-ms [&_p]:s:text-m">
                     <TinaMarkdown content={description} />
                 </div>
             )}

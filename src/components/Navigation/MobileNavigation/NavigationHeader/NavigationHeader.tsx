a60bc0c9f0f5cc31d17945c58db98ab9ad30f930
Michal Rygorowicz <mitsugi23@hotmail.com>
2024-10-05 11:54:15 +0200

    apply new styles for mobile menu

--- src/components/Navigation/MobileNavigation/NavigationHeader/NavigationHeader.tsx
+++
@@ -1,7 +0,0 @@
-export default function NavigationHeader() {
-    return (
-        <div className="flex items-center justify-start bg-supplementary-3 px-4 py-4">
-            <h1 className="text-2xl uppercase text-leading-main-2">Menu</h1>
-        </div>
-    );
-}
